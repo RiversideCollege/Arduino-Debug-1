46db589f4bf2748e47505a7492e3f15a4ad3a0e6
David Petticrew <david.petticrew@riversidecollege.ac.uk>
2017-09-17 21:21:48 +0100

    Code to debug

---
+++ debug1/debug1.ino
@@ -0,0 +1,11 @@
+int LED=2;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinmode(led,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalwrite(Led,high)
+}
